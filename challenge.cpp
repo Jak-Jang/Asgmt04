b8b98764661ef4cc2b13c6803a1453c01483ffc8
Jak-Jang <jjgk24211@gmail.com>
2025-06-13 19:35:55 +0900

    newChallenge

--- challenge.cpp
+++ challenge.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
 
 using namespace std; // namespace std 사용
 
@@ -20,53 +20,11 @@ private:
     vector<Book> books; // 책 목록 저장
 
 public:
-    Book* findBookByTitle(string title)
-    {
-        size_t idx = 0;
-        for (; idx < books.size(); idx++)
-        {
-            if (books[idx].title == title)
-            {
-                return &books[idx];
-            }
-        }
-        return nullptr; 
-    }
-
-    Book* findBookByAuthor(string author)
-    {
-        size_t idx = 0;
-        for (; idx < books.size(); idx++)
-        {
-            if (books[idx].author == author)
-            {
-                return &books[idx];
-            }
-        }
-        return nullptr;
-    }
-
     // 책 추가 메서드
     void addBook(const string& title, const string& author) 
     {
-        bool valid = false;
-        for (size_t i = 0; i < books.size(); i++)
-        {
-            if (books[i].title == title)
-            {
-                valid = true;
-                break;
-            }
-        }
-        if (valid)
-        {
-            cout << "중복된 도서명입니다. 추가할 수 없습니다." << endl;
-        }
-        else
-        {
-            books.push_back(Book(title, author)); // push_back 사용
-            cout << "책이 추가되었습니다: " << title << " by " << author << endl;
-        }
+        books.push_back(Book(title, author)); // push_back 사용
+        cout << "책이 추가되었습니다: " << title << " by " << author << endl;
     }
 
     // 모든 책 출력 메서드
@@ -79,56 +37,135 @@ public:
         }
 
         cout << "현재 도서 목록:" << endl;
-        for (size_t i = 0; i < books.size(); i++) // 일반적인 for문 사용
-        {   // size_t == unsigned integer 부호 없는 정수
+        for (size_t i = 0; i < books.size(); i++) 
+        { // 일반적인 for문 사용
             cout << "- " << books[i].title << " by " << books[i].author << endl;
         }
     }
+
+    void searchByTitle(string& title)
+    {
+        for (Book& book : books)
+        {
+            if (book.title == title)
+            {
+                cout << "도서명: " << title << "을(를) 찾았습니다." << endl;
+                return;
+            }
+        }
+
+        cout << title << "라는 책을 찾지 못했습니다." << endl;
+    }
+
+    void searchByAuthor(string& author)
+    {
+        for (Book& book : books)
+        {
+            if (book.author == author)
+            {
+                cout << "저자: " << author << "을(를) 찾았습니다." << endl;
+                return;
+            }
+        }
+
+        cout << "저자 '" << author << "'의 책을 찾지 못했습니다." << endl;
+    }
+
+    Book* findBookByTitle(const string& title)
+    {
+        for (Book& book : books)
+        {
+            if (book.title == title)
+            {
+                return &book;
+            }
+        }
+
+        return nullptr;
+    }
+
+    Book* findBookByAuthor(const string& author)
+    {
+        for (Book& book : books)
+        {
+            if (book.author == author)
+            {
+                return &book;
+            }
+        }
+
+        return nullptr;
+    }
+
 };
 
 class BorrowManager
 {
 private:
-    unordered_map<string, int> stock; // 중복X 도서명, 권수
+    unordered_map<string, int> stock;
 
 public:
-    void initializeStock(string book, int quantity = 3)
+    void initializeStock(const Book* book, int quantity = 3)
     {
-        stock[book] = quantity;
+        if (nullptr == book)
+        {
+            return;
+        }
+
+        stock[book->title] = quantity;
+        cout << "책이 추가되었습니다." << endl;
     }
 
-    void borrowBook(string title)
+    void borrowBook(const string& title)
     {
-        if (stock.find(title) != stock.end() && stock[title] != 0)
+        if (stock.end() != stock.find(title))
         {
-            stock[title] -= 1;
-            cout << title << " 대출 완료" << endl;
+            if (stock[title] > 0)
+            {
+                --stock[title];
+                cout << title << "을(를) 대여했습니다." << endl;
+            }
+            else
+            {
+                cout << "재고 부족 / 대여 실패" << endl;
+            }
         }
         else
         {
-            cout << "도서 대출 실패" << endl;
+            cout << "일치하는 책이 없습니다." << endl;
         }
     }
 
-    void returnBook(string title)
+    void returnBook(const string& title)
     {
-        if (stock.find(title) != stock.end() && stock[title] != 3)
+        if (stock.end() != stock.find(title))
         {
-            stock[title] += 1;
-            cout << title << " 반납 완료" << endl;
+            if (stock[title] < 3)
+            {
+                ++stock[title];
+                cout << title << "을(를) 반납했습니다." << endl;
+            }
+            else
+            {
+                cout << "반납 실패" << endl;
+            }
         }
         else
         {
-            cout << "도서 반납 실패" << endl;
+            cout << "일치하는 책이 없습니다." << endl;
         }
     }
 
     void displayStock()
     {
+        for (pair<const string, int> e : stock)
+        {
+            cout << "책 제목: " << e.first << " / 재고: " << e.second << endl;
+        }
         for (auto it : stock)
         {
-            cout << "'" << it.first << "'의 잔여량 : ";
-            cout << it.second << "/3" << endl;
+            cout << it.first << " " << it.second << endl;
+            cout << &it.first;
         }
     }
 };
@@ -145,10 +182,9 @@ int main()
         cout << "\n도서관 관리 프로그램" << endl;
         cout << "1. 책 추가" << endl; // 책 정보를 입력받아 책 목록에 추가
         cout << "2. 모든 책 출력" << endl; // 현재 책 목록에 있는 모든 책 출력
-        cout << "3. 도서 검색" << endl; // 도서 검색 분기점
-        cout << "4. 도서 대출 현황" << endl;
-        cout << "5. 도서 대출 / 반납" << endl;
-        cout << "6. 종료" << endl; // 프로그램 종료
+        cout << "3. 검색" << endl;
+        cout << "4. 대여/반납" << endl;
+        cout << "5. 종료" << endl; // 프로그램 종료
         cout << "선택: ";
 
         int choice; // 사용자의 메뉴 선택을 저장
@@ -160,95 +196,101 @@ int main()
             // 사용자로부터 책 제목과 저자명을 입력받아 BookManager에 추가합니다.
             string title, author;
             cout << "책 제목: ";
-            cin.ignore(); // 이전 입력의 잔여 버퍼를 제거 = 개행 문자 제거
+            cin.ignore(); // 이전 입력의 잔여 버퍼를 제거
             getline(cin, title); // 제목 입력 (공백 포함)
             cout << "책 저자: ";
             getline(cin, author); // 저자명 입력 (공백 포함)
             manager.addBook(title, author); // 입력받은 책 정보를 추가
-            borrow.initializeStock(title);
+            borrow.initializeStock(manager.findBookByTitle(title));
         }
         else if (choice == 2) 
         {
             // 2번 선택: 모든 책 출력
             // 현재 BookManager에 저장된 책 목록을 출력합니다.
             manager.displayAllBooks();
+            borrow.displayStock();
         }
         else if (choice == 3)
         {
-            // 3번 선택: 도서 검색 분기점
-            cout << "1. 도서명 검색" << endl;
-            cout << "2. 작가명 검색" << endl;
-            cout << "선택: ";
+            cout << "1. 도서명 / 2. 저자명" << endl;
+            cin >> choice;
+            string title, author;
+            if (choice == 1)
+            {
+                cin.ignore();
+                getline(cin, title);
+                manager.searchByTitle(title);
+            }
+            else if (choice == 2)
+            {
+                cin.ignore();
+                getline(cin, author);
+                manager.searchByAuthor(author);
+            }
+            else
+            {
+                cout << "메인으로 돌아갑니다." << endl;
+            }
+        }
+        else if (choice == 4)
+        {
+            cout << "1. 대출 / 2. 반납" << endl;
+            cin >> choice;
             string temp;
-            cin >> choice;
             if (choice == 1)
             {
-                cout << "도서명 입력: ";
+                cout << "도서명 또는 저자명을 입력하세요." << endl;
                 cin.ignore();
                 getline(cin, temp);
-                if (manager.findBookByTitle(temp) != nullptr)
+                Book* book = manager.findBookByTitle(temp);
+                if (book)
                 {
-                    cout << manager.findBookByTitle(temp)->title << " by ";
-                    cout << manager.findBookByTitle(temp)->author << endl;
+                    borrow.borrowBook(book->title);
                 }
                 else
                 {
-                    cout << "검색 실패. 메인으로 돌아갑니다." << endl;
+                    book = manager.findBookByAuthor(temp);
+                    if (book)
+                    {
+                        borrow.borrowBook(book->title);
+                    }
+                    else
+                    {
+                        cout << "일치하는 책이 없습니다." << endl;
+                    }
                 }
-
             }
             else if (choice == 2)
             {
-                cout << "작가명 입력: ";
+                cout << "도서명 또는 저자명을 입력하세요." << endl;
                 cin.ignore();
                 getline(cin, temp);
-                if (manager.findBookByAuthor(temp) != nullptr)
+                Book* book = manager.findBookByTitle(temp);
+                if (book)
                 {
-                    cout << manager.findBookByAuthor(temp)->title << " by ";
-                    cout << manager.findBookByAuthor(temp)->author << endl;
+                    borrow.returnBook(book->title);
                 }
                 else
                 {
-                    cout << "검색 실패. 메인으로 돌아갑니다." << endl;
+                    book = manager.findBookByAuthor(temp);
+                    if (book)
+                    {
+                        borrow.returnBook(book->title);
+                    }
+                    else
+                    {
+                        cout << "일치하는 책이 없습니다." << endl;
+                    }
                 }
             }
             else
             {
-                cout << "잘못된 입력입니다. 메인으로 돌아갑니다." << endl;
+                cout << "메인으로 돌아갑니다." << endl;
             }
         }
-        else if (choice == 4) // 도서 대출 현황
+        else if (choice == 5) 
         {
-            borrow.displayStock();
-        }
-        else if (choice == 5) // 도서 대출/반납
-        {
-            cout << "1. 도서 대출" << endl;
-            cout << "2. 도서 반납" << endl;
-            cout << "선택: ";
-            cin >> choice;
-            string title;
-            if (choice == 1)
-            {
-                cout << "대출할 도서명 입력: ";
-                cin.ignore();
-                getline(cin, title);
-                borrow.borrowBook(title);
-            }
-            else if (choice == 2)
-            {
-                cout << "반납할 도서명 입력: ";
-                cin.ignore();
-                getline(cin, title);
-                borrow.returnBook(title);
-            }
-            else
-            {
-                cout << "잘못된 입력입니다. 메인으로 돌아갑니다." << endl;
-            }
-        }
-        else if (choice == 6)
-        {
+            // 3번 선택: 종료
             // 프로그램을 종료하고 사용자에게 메시지를 출력합니다.
             cout << "프로그램을 종료합니다." << endl;
             break; // while 루프 종료
@@ -259,6 +301,8 @@ int main()
             // 메뉴에 없는 번호를 입력했을 경우 경고 메시지를 출력합니다.
             cout << "잘못된 입력입니다. 다시 시도하세요." << endl;
         }
+
+        system("pause");
     }
 
     return 0; // 프로그램 정상 종료
